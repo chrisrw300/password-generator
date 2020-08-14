12ded8f286ca54fb69382a208854d10445d0278e
Chris Walston <Chris@myaccountant.local>
2020-08-14 15:01:26 -0700

    added variables and arrays

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,31 @@
-// Assignment code here
+// Variables for Password Generator
+var passwordSpecialChar;
+var passwordNumber;
+var passwordUpperChar;
+var passwordLowerChar;
+
+var passwordLength = "";
+
+
+// Array for Password Generator
+var upperChar = [
+  "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"
+  ];
+
+var lowerChar = [
+  "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"
+  ];
+
+var specialChar = [
+  "!","@","#","$","%","^","&","*","(",")","-","_","=","+","`","~","[","]","{","}","?",";",":","<",">"
+  ];
+
+var number = [
+  "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"
+];
+
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
